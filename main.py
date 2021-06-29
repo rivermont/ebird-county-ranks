be1d266f382a150878a473e9829d416e4391b73a
Will Bennett <william.11bennett@gmail.com>
2021-06-29 19:49:02 -0400

    Update with working version rewritten today.
    Reused the bs4 code but rewrote almost the entire rest of script from scratch. Instead of downloading the county list from the API, it is now already saved in file counties.json to be parsed. I toyed with using the Google Sheets API to input values directly into an online spreadsheet, but creating a csv from scratch is probably faster and definitely uses less bandwidth.

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-# This script will output a spreadsheet of the frequency rank of every species found in every county
-# final spreadsheet will have counties as entries/rows and species as fields/columns
-
-from ast import literal_eval
 import requests
+import json
 from bs4 import BeautifulSoup
 from time import sleep
-import json
 
-key = 'XXXXXXXXXXX'  # eBird API key goes here
-session = 'XXXXXXXXXXXXXXXXXXX'  # ebird.org session cookie goes here
-headers = {"X-eBirdApiToken": key}
-cookies = {"EBIRD_SESSIONID": session}
+
+
+url = "https://ebird.org/targets?r2=AQ&r1="
+cookies = {'EBIRD_SESSIONID': 'F4BA26BD1675CFC151862954CA1758C2'}
+
+final_sp = set()
 
 
 def main():
-    # get list of all counties
-    response = requests.get("https://api.ebird.org/v2/ref/region/list/subnational2/US", headers=headers)
-    all_counties = literal_eval(response.text)
-    del response
-    # all_counties = [{"code": "US-NC-113", "name": "Macon"},
-    #                 {"code": "US-CO-107", "name": "Routt"},
-    #                 {"code": "US-IL-187", "name": "Warren"}]
+    global final_sp
 
-    # sort by region code
-    all_counties.sort(key=lambda item: item.get("code"))
-    # print(all_counties)
+    # parse county list from file
 
-    for county in all_counties:
-        # get target page
-        response = requests.get("https://ebird.org/targets?r1={0}&r2=AQ".format(county["code"]), cookies=cookies)
+    with open('counties.json') as open_file:
+        data = json.load(open_file)
+
+    # counter = 0
+    for county in data:
+        # counter += 1
+        
+        county.update({'sp': {}})
+        
+        response = requests.get(f'{url}{county["code"]}', cookies=cookies)
+        # parse page
 
         # scrape the page for the species and ranks
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -41,31 +39,77 @@ def main():
             rank = div1.get_text().split()[0][:-1]
             # species = content of tag data-species-code of > div class=ResultsStats-title > div > h5 > a
             species = div1.find(attrs={"data-species-code": True})['data-species-code']
+            final_sp.add(species)
             # frequency =  content of title tag of > div class ResultsStats-stats
             freq = div1.find(class_="ResultsStats-stats")['title'][:-11]
 
-            county.update({species: {"rank": rank, "frequency": freq}})
+            county['sp'].update({species: {"rank": rank, "frequency": freq}})
 
         print(county["code"] + " " + county["name"])  # where we at
+        
+        
+        # if counter > 3:
+        #     break
+        
         sleep(1)  # rate limit
-        break  # uncomment to only run on the first county
+    
+    final_sp = list(final_sp)  # lock species list in to final order
+    
+    return data
 
-    if usefile:
-        with open('output.txt', 'w+') as file:
-            file.write(str(all_counties))
-    else:
-        # create and save spreadsheet
-        # possibly in a separate script and possibly using google sheets api
-        pass
+
+def create_file(data):
+    """
+    Write values to final output file.
+    """
+    global final_sp
+    final = ",Species:,"
+    
+    print('Adding species to file contents...')
+    
+    for sp in final_sp:
+        final += f'{sp},'
+    final += '\n'
+    
+    print('Adding counties to file contents...')
+    
+    c = ','
+    
+    for county in data:
+        final += f'{county["code"]},{county["name"] + " " + county["code"][3:5]}'
+        
+        # construct string containing species ranks
+        
+        sp_str = c * len(final_sp)
+        
+        for sp in county['sp']:
+            # get current comma positions
+            positions = [pos for pos, char in enumerate(sp_str) if char == c]
+            # print(positions)
+            # print(final_sp)
+            
+            # get position of sp in final_sp
+            p = final_sp.index(sp)
+            p1 = positions[p] + 1
+            
+            # write sp['rank'] to correct position
+            sp_str = sp_str[:p1] + county['sp'][sp]['rank'] + sp_str[p1:]
+
+        final += sp_str
+        
+        final += '\n'
+    
+    
+    print('Writing file...')
+    with open('counties_spreadsheet.csv', 'w+') as open_file:
+        open_file.write(final)
+    
+    
+    # print(final)
 
 
 if __name__ == "__main__":
-    usefile = 1  # 0: write spreadsheet  1: write dict to file  2: read from old file
-    main()
-    if usefile == 2:
-        with open('output.txt', 'r') as file:
-            data = file.read()
-        # data = literal_eval(data)
-        print(data[:50])
-        data = json.loads(data[1:-1])
-        print(type(data))
+    data = main()
+    
+    create_file(data)
+    
